015c9e67c7597e1cc249d7044d2f2ccf316736d1
thisangaThewanmi <thisangat2005@gmail.com>
2025-02-22 05:34:09 +0530

    Changed the entry component after routing process

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Customer from "./pages/Staff.tsx";
-import BakeryItems from "./pages/BakeryItems.tsx";
+
+import DashboardPage from "./pages/DashboardPage.tsx";
 // import './App.css'
 
 function App() {
 
   return (
 
-       <Customer></Customer>
-      // <BakeryItems/>
+       <DashboardPage/>
 
   )
 }
