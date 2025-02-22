4ba6bb63b1c723be3b4b04c7f53ca20473423480
thisangaThewanmi <thisangat2005@gmail.com>
2025-02-22 05:32:22 +0530

    Added react router and did changes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import {createBrowserRouter, RouterProvider} from "react-router";
+import Dashboard from "./pages/DashboardPage.tsx";
+import Items from "./pages/Items.tsx";
+import Staff from "./pages/Staff.tsx";
+import Order from "./pages/Order.tsx";
+
+
+const router = createBrowserRouter(
+    [
+        {path:"/",element:<Dashboard/>},
+        {path:"/item",element:<Items/>},
+        {path:"/staff",element:<Staff/>},
+        {path:"/order",element:<Order/>}
+    ]
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
